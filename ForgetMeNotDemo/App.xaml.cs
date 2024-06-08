dfddbe803f8ff81304cce995d053104b1f25c252
devops.cloud.gporeyma <devops.cloud.gporeyma@hotmail.com>
2024-06-08 10:28:39 -0600

    Add project files.

---
+++ ForgetMeNotDemo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ForgetMeNotDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
